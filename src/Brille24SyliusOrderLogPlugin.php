c4c2b4f5c50024897c6e466b6a46de8a873f88d6
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2020-08-21 08:53:02 +0200

    Initial commit

---
+++ src/Brille24SyliusOrderLogPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brille24\SyliusOrderLogPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class Brille24SyliusOrderLogPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
