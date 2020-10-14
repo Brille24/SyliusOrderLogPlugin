a1cc77cce8d79b4fd78f01f3679c6c949a73c005
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2020-10-14 10:26:40 +0200

    Update to Sylius 1.8

--- src/Migrations/Version20200824080653.php
+++ src/Migrations/Version20200824080653.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Migrations;
+namespace Brille24\SyliusOrderLogPlugin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
