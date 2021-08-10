227e394341b38010333d0a7772a05775a6e52284
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2021-08-10 09:38:44 +0200

    Update 1.10

--- src/DependencyInjection/Brille24SyliusOrderLogExtension.php
+++ src/DependencyInjection/Brille24SyliusOrderLogExtension.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Brille24\SyliusOrderLogPlugin\DependencyInjection;
 
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -12,35 +14,41 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class Brille24SyliusOrderLogExtension extends Extension implements PrependExtensionInterface
 {
+    use PrependDoctrineMigrationsTrait;
+
     /**
-     * {@inheritdoc}
+     * @psalm-suppress UnusedVariable
      */
-    public function load(array $config, ContainerBuilder $container): void
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.xml');
     }
 
-    public function prepend(ContainerBuilder $container): void
+    public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
     {
-        if (!$container->hasExtension('doctrine_migrations') || !$container->hasExtension(
-            'sylius_labs_doctrine_migrations_extra'
-        )) {
-            return;
-        }
+        return new Configuration();
+    }
 
-        $container->prependExtensionConfig('doctrine_migrations', [
-            'migrations_paths' => [
-                'Brille24\SyliusOrderLogPlugin\Migrations' => '@Brille24SyliusOrderLogPlugin/Migrations',
-            ],
-        ]);
+    protected function getMigrationsNamespace(): string
+    {
+        return 'Brille24\SyliusOrderLogPlugin\Migrations';
+    }
 
-        $container->prependExtensionConfig('sylius_labs_doctrine_migrations_extra', [
-            'migrations' => [
-                'Brille24\SyliusOrderLogPlugin\Migrations' => ['Sylius\Bundle\CoreBundle\Migrations'],
-            ],
-        ]);
+    protected function getMigrationsDirectory(): string
+    {
+        return '@Brille24SyliusOrderLogPlugin/Migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return ['Sylius\Bundle\CoreBundle\Migrations'];
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $this->prependDoctrineMigrations($container);
     }
 }
